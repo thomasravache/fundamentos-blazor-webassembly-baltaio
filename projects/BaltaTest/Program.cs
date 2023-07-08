743a6796c668e95320081c448d9ebc53569fb8a1
Thomas Ravache <thomasravache31@gmail.com>
2023-07-08 12:16:08 -0300

    09. App e HeadOutlet

--- projects/BaltaTest/Program.cs
+++ projects/BaltaTest/Program.cs
@@ -3,9 +3,11 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BaltaTest;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// Procura o elemento html dentro do index.html que tem o id "app"
+builder.RootComponents.Add<App>("#app");
+
+// Permite que o título da página seja trocada com o <PageTitle></PageTitle> no Index.razor
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
 await builder.Build().RunAsync();
